7503cb8ac2d5248cbea95ab06c564371bb915370
jdavid18 <jdavid18@gmail.com>
2023-12-19 17:41:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-// script.js
 function submitForm() {
     const email = document.getElementById('email').value;
     const subject = document.getElementById('subject').value;
     const message = document.getElementById('message').value;
     const phone = document.getElementById('phone').value;
 
-    // You can perform additional validation here
 
     const formData = `Email: ${email}\nSubject: ${subject}\nMessage: ${message}\nPhone Number: ${phone}`;
     console.log(formData);
-    // Replace with your Telegram bot token and channel ID
+    telegramMessage(formData);
+   
+}
+
+function telegramMessage(formData){
     const botToken = '6580613461:AAFFrCbdwwKrF8sXXoSLb3JOJLcrVecAwuY';
     const channelID = '-1002019424375';
 
-    // Send message to Telegram channel
     fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${channelID}&text=${encodeURIComponent(formData)}`, {
         method: 'GET',
     })
@@ -30,22 +31,18 @@ function submitForm() {
 }
 
 function parseCurrentURL() {
-    // Get the current URL
     const currentURL = window.location.href;
 
-    // Parse the URL to extract parameters
     const urlParts = currentURL.split('?');
     if (urlParts.length > 1) {
         const queryString = urlParts[1];
         const queryParams = new URLSearchParams(queryString);
 
-        // Extract parameters
         const name = queryParams.get('Name') || '';
         const email = queryParams.get('email') || '';
         const subject = queryParams.get('subject') || '';
         const message = queryParams.get('message') || '';
 
-        // Create the final output
         const formData = `Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
 
         return formData;
@@ -54,8 +51,6 @@ function parseCurrentURL() {
     }
 }
 
-// Call the function
 const result = parseCurrentURL();
-
-// Print the final output
 console.log(result);
+telegramMessage(result);
